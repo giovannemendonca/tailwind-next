b89203b4230dee5483e0ec6cb0d8a51457c80e47
Giovanne Mendonca <giovannemendonca@outlook.com>
2023-08-12 08:38:58 -0300

    create sidebar

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Logo } from './logo'
+import {
+  BarChart,
+  CheckSquare,
+  Cog,
+  Flag,
+  Home,
+  LifeBuoy,
+  Search,
+  SquareStack,
+  User,
+} from 'lucide-react'
+import { NavItem } from './NavItem'
+import UsedSpaceWidget from './UsedSpaceWidget'
+import { Profile } from './Profile'
+
+export function Sidebar() {
+  return (
+    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
+      <Logo />
+      <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
+        <Search className="h-5 w-5 text-zinc-500" />
+        <input
+          type="text"
+          className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 "
+          placeholder="Search"
+        />
+      </div>
+      <nav className="space-y-0.5">
+        <NavItem title="Home" icon={Home} />
+        <NavItem title="Dashboard" icon={BarChart} />
+        <NavItem title="Projects" icon={SquareStack} />
+        <NavItem title="Tasks" icon={CheckSquare} />
+        <NavItem title="Reporting" icon={Flag} />
+        <NavItem title="Users" icon={User} />
+      </nav>
+      <div className="mt-auto flex flex-col gap-6">
+        <nav className="space-y-6">
+          <NavItem title="Suport" icon={LifeBuoy} />
+          <NavItem title="Setting" icon={Cog} />
+        </nav>
+        <UsedSpaceWidget />
+        <div className="h-px bg-zinc-200"></div>
+        <Profile />
+      </div>
+    </aside>
+  )
+}
